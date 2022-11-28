0591f53fcaf64697da2de929886abf9fc14c9709
Pawil <114557817+Pawi1@users.noreply.github.com>
2022-11-28 14:15:35 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,45 +1,57 @@
 ﻿using System;
 namespace TicTacToe
-{internal class Program{
+
+{
+  internal class Program{
         static void Main(string[] args)
-        { char a1=' ',b1=' ',c1=' ',a2=' ',b2=' ',c2=' ',a3=' ',b3=' ',c3=' ';  
+        { Console.Clear();
+          char a1=' ',b1=' ',c1=' ',a2=' ',b2=' ',c2=' ',a3=' ',b3=' ',c3=' ';  
             string w ="";
+            int a=0;
             for (int q=1 ;q<=9;q++)
             { if(q==1||q==3||q==5||q==7||q==9) w="kółko";
               else w="krzyżyk";
               if (q==1)DajPlansze();
+              if (a1=='○' && b1=='○' && c1=='○' || a2=='○' && b2=='○' && c2=='○' ||  a3=='○' && b3=='○' && c3=='○' || a3=='○' && b3=='○' && c3=='○' || b1=='○' && b2=='○' && b3=='○'|| c1=='○' && c2=='○' && c3=='○'||a1=='○' && b2=='○' && c3=='○' || a3=='○' && b2=='○' && c1=='○') a=1;               
+              if (a1=='x' && b1=='x' && c1=='x' || a2=='x' && b2=='x' && c2=='x' ||  a3=='x' && b3=='x' && c3=='x' || a3=='x' && b3=='x' && c3=='x' || b1=='x' && b2=='x' && b3=='x'|| c1=='x' && c2=='x' && c3=='x'||a1=='x' && b2=='x' && c3=='x' || a3=='x' && b2=='x' && c1=='x') a=1;               
+              if (a!=0) break;
               Console.Write($"\n\t   Kolej gracza: {w}: ");
               IdzDo:
               ConsoleKeyInfo o = Console.ReadKey();
                 if (w=="kółko")
               {
               if (o.Key == ConsoleKey.Q ||o.Key == ConsoleKey.NumPad7 || a1==' ') a1='○';
-              else if (o.Key == ConsoleKey.W ||o.Key == ConsoleKey.NumPad8 || b1==' ') b1='○';
-              else if (o.Key == ConsoleKey.E ||o.Key == ConsoleKey.NumPad9 || c1==' ') c1='○';
-              else if (o.Key == ConsoleKey.A ||o.Key == ConsoleKey.NumPad4 || a2==' ') a2='○';
-              else if (o.Key == ConsoleKey.S ||o.Key == ConsoleKey.NumPad5 || b2==' ') b2='○';
-              else if (o.Key == ConsoleKey.D ||o.Key == ConsoleKey.NumPad6 || c2==' ') c2='○';
-              else if (o.Key == ConsoleKey.Z ||o.Key == ConsoleKey.NumPad1 || a3==' ') a3='○';
-              else if (o.Key == ConsoleKey.X ||o.Key == ConsoleKey.NumPad2 || b3==' ') b3='○';
-              else if (o.Key == ConsoleKey.C ||o.Key == ConsoleKey.NumPad3 || c3==' ') c3='○';
+              else if (o.Key == ConsoleKey.W ||o.Key == ConsoleKey.NumPad8 && b1==' ') b1='○';
+              else if (o.Key == ConsoleKey.E ||o.Key == ConsoleKey.NumPad9 && c1==' ') c1='○';
+              else if (o.Key == ConsoleKey.A ||o.Key == ConsoleKey.NumPad4 && a2==' ') a2='○';
+              else if (o.Key == ConsoleKey.S ||o.Key == ConsoleKey.NumPad5 && b2==' ') b2='○';
+              else if (o.Key == ConsoleKey.D ||o.Key == ConsoleKey.NumPad6 && c2==' ') c2='○';
+              else if (o.Key == ConsoleKey.Z ||o.Key == ConsoleKey.NumPad1 && a3==' ') a3='○';
+              else if (o.Key == ConsoleKey.X ||o.Key == ConsoleKey.NumPad2 && b3==' ') b3='○';
+              else if (o.Key == ConsoleKey.C ||o.Key == ConsoleKey.NumPad3 && c3==' ') c3='○';
               else goto IdzDo;
               }
                 if (w=="krzyżyk")
               {
               if (o.Key == ConsoleKey.Q ||o.Key == ConsoleKey.NumPad7 || a1==' ') a1='x';
-              else if (o.Key == ConsoleKey.W ||o.Key == ConsoleKey.NumPad8 || b1==' ') b1='x';
-              else if (o.Key == ConsoleKey.E ||o.Key == ConsoleKey.NumPad9 || c1==' ') c1='x';
-              else if (o.Key == ConsoleKey.A ||o.Key == ConsoleKey.NumPad4 || a2==' ') a2='x';
-              else if (o.Key == ConsoleKey.S ||o.Key == ConsoleKey.NumPad5 || b2==' ') b2='x';
-              else if (o.Key == ConsoleKey.D ||o.Key == ConsoleKey.NumPad6 || c2==' ') c2='x';
-              else if (o.Key == ConsoleKey.Z ||o.Key == ConsoleKey.NumPad1 || a3==' ') a3='x';
-              else if (o.Key == ConsoleKey.X ||o.Key == ConsoleKey.NumPad2 || b3==' ') b3='x';
-              else if (o.Key == ConsoleKey.C ||o.Key == ConsoleKey.NumPad3 || c3==' ') c3='x';
+              else if (o.Key == ConsoleKey.W ||o.Key == ConsoleKey.NumPad8 && b1==' ') b1='x';
+              else if (o.Key == ConsoleKey.E ||o.Key == ConsoleKey.NumPad9 && c1==' ') c1='x';
+              else if (o.Key == ConsoleKey.A ||o.Key == ConsoleKey.NumPad4 && a2==' ') a2='x';
+              else if (o.Key == ConsoleKey.S ||o.Key == ConsoleKey.NumPad5 && b2==' ') b2='x';
+              else if (o.Key == ConsoleKey.D ||o.Key == ConsoleKey.NumPad6 && c2==' ') c2='x';
+              else if (o.Key == ConsoleKey.Z ||o.Key == ConsoleKey.NumPad1 && a3==' ') a3='x';
+              else if (o.Key == ConsoleKey.X ||o.Key == ConsoleKey.NumPad2 && b3==' ') b3='x';
+              else if (o.Key == ConsoleKey.C ||o.Key == ConsoleKey.NumPad3 && c3==' ') c3='x';
               else goto IdzDo;
               }
+
+                
                 Console.Clear();
                 DajPlansze(a1,b1,c1,a2,b2,c2,a3,b3,c3);
             }
+            if(a==1)Console.WriteLine($"Wygrał {w}");
+            else Console.WriteLine("Remis");
+            
         }        
         static void DajPlansze(char a1=' ',char b1=' ',char c1=' ',char a2=' ',char b2=' ',char c2=' ',char a3=' ',char b3=' ',char c3=' ')
         {
